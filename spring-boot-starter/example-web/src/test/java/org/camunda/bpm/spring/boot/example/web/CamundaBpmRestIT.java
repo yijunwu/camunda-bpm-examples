4a7821133f60e5ee4eb0c137d0255ef439216e85
Tassilo Weidner <tassilo.weidner@camunda.com>
2018-05-11 16:14:17 +0200

    chore(spring boot): adjust and migrate examples to 7.9 - bump spring boot to 2.0.0 - bump camunda spring boot starter to 3.0.0
    related to CAM-9031

--- spring-boot-starter/example-web/src/test/java/org/camunda/bpm/spring/boot/example/web/CamundaBpmRestIT.java
+++ spring-boot-starter/example-web/src/test/java/org/camunda/bpm/spring/boot/example/web/CamundaBpmRestIT.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
